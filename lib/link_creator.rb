7597d10e4dfb29196db58009338ab9ab398e57a8
Eugene Sypachev <eugene.sypachev@gmail.com>
2012-08-03 00:13:53 +0400

    update! simple config and redmine 2.0 support

--- lib/link_creator.rb
+++ lib/link_creator.rb
@@ -5,14 +5,9 @@ module RedmineTopBarLinks
     def self.link_list
       list = {}
       yml = YAML.load_file "#{File.dirname(__FILE__)}/../config.yml"
-      yml.each_key do |key|
-        if key.match /^link/ui
-          name = yml[key]['name']
-          link = yml[key]['path']
-          puts "#{name}, #{link} \n"
-          list[name] = link
-        end
-      end
+      yml.each_pair { |key, value|
+        list[key] = value
+      }
       list
     end
   end
